48d6103f640c10a02c1de0a7b329f76fbb6aa043
Napv2900 <114603384+Napv2900@users.noreply.github.com>
2022-11-17 09:38:26 +0700

    Update прог.py

--- прог.py
+++ прог.py
@@ -12,22 +12,22 @@ def converter():
                       "\n I-информационный объём текста\n"))
         if x == "N":
             i = int(input("Введите значение i(битов): "))
-            print(2 ** i)
+            print(2 ** i" битов")
         if x == "i":
             N = int(input("Введите значение N(битов), если оно известно, если нет, то 0: "))
             if N != 0:
-                print(math.log2(N))
+                print(math.log2(N)" битов")
             else:
                 I = int(input("Введите значение I(битов): "))
                 K = int(input("Введите значение K(кол-во символов): "))
-                print(I // K)
+                print(I // K" битов")
         if x == "I":
             K = int(input("Введите значение K(кол-во символов): "))
             i = int(input("Введите значение i(битов), если оно известно, если нет, то 0: "))
             if i != 0:
-                print(K * i)
+                print(K * i" битов")
             else:
-                print(K * math.log2(N))
+                print(K * math.log2(N)" битов")
         if x == "K":
             I = int(input("Введите значение I(битов): "))
             i = int(input("Введите значение i(битов), если оно известно, если нет, то 0: "))
@@ -49,39 +49,39 @@ def converter():
         if x == "b":
             K = int(input("Введите значение K(кол-во ур. квант. звука), если оно извсетно, если нет, то 0: "))
             if K != 0:
-                print(math.log2(K))
+                print(math.log2(K)" битов")
             else:
                 I = int(input("Введите значение I(битов): "))
                 H = int(input("Введите значение H(Гц): "))
                 t = int(input("Введите значение t(с): "))
-                print(I / (H * t))
+                print(I / (H * t)" битов")
         if x == "I":
             H = int(input("Введите значение H(Гц): "))
             t = int(input("Введите значение t(с): "))
             b = int(input("Введите значение b(битов), если оно извсетно, если нет, то 0: "))
             if b != 0:
-                print(H * t * b)
+                print(H * t * b" битов")
             else:
                 K = int(input("Введите значение K(кол-во ур. квант. звука): "))
-                print(H * t * math.log2(K))
+                print(H * t * math.log2(K)" битов")
         if x == "H":
             I = int(input("Введите значение I(битов): "))
             t = int(input("Введите значение t(с): "))
             b = int(input("Введите значение b(битов), если оно извсетно, если нет, то 0: "))
             if b != 0:
-                print(I / (t * b))
+                print(I / (t * b)" Гц")
             else:
                 K = int(input("Введите значение K(кол-во ур. квант. звука): "))
-                print(I / (t * math.log2(K)))
+                print(I / (t * math.log2(K))" Гц")
         if x == "t":
             I = int(input("Введите значение I(битов): "))
             H = int(input("Введите значение H(Гц): "))
             b = int(input("Введите значение b(битов), если оно извсетно, если нет, то 0: "))
             if b != 0:
-                print(I / (H * b))
+                print(I / (H * b)" с")
             else:
                 K = int(input("Введите значение K(кол-во ур. квант. звука): "))
-                print(I / (H * math.log2(K)))
+                print(I / (H * math.log2(K))" с")
 
     def image():
         x = str(input("Что вы хотите найти?"
@@ -92,7 +92,7 @@ def converter():
             print(2 ** b)
         if x == "b":
             K = int(input("Введите значение K(кол-во оттенков): "))
-            print(math.log2(K))
+            print(math.log2(K)" битов")
     if number==1:
         infa()
     if number==2:
